a19882cf2daae321d4ac8105a5874b8eb8c82f42
Chris Benoit <chris.benoit@autodesk.com>
2014-03-12 00:22:49 -0400

    blah

--- php/index.php
+++ php/index.php
@@ -40,8 +40,8 @@
 
 <video width="768" height="574" controls autoplay>
 <!--<source src="http://dl.dropboxusercontent.com/u/19417682/promenadeapp.com/mov/walkthroughHB.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>-->
-<source src="https://dl.dropboxusercontent.com/u/19417682/promenadeapp.com/mov/walkthroughHB.webmsd.webm" type='video/webm; codecs="vp8, vorbis"'>
-<!--<source src="http://dl.dropboxusercontent.com/u/19417682/promenadeapp.com/mov/walkthroughHB.ogv" type='video/ogg; codecs="theora, vorbis"'>-->
+<!---<source src="https://dl.dropboxusercontent.com/u/19417682/promenadeapp.com/mov/walkthroughHB.webmsd.webm" type='video/webm; codecs="vp8, vorbis"'>-->
+<source src="http://dl.dropboxusercontent.com/u/19417682/promenadeapp.com/mov/walkthroughHB.ogv" type='video/ogg; codecs="theora, vorbis"'>
 </video>
 
 </center>
